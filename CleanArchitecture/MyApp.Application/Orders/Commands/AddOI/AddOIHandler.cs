ec9ff4bf57c7b03795a630fd3de13f75fa9e6ca6
Onebcg-sanket <sanket.kumar@onebcg.com>
2025-07-23 15:15:20 +0530

    Comment: Test case 4.

--- CleanArchitecture/MyApp.Application/Orders/Commands/AddOI/AddOIHandler.cs
+++ CleanArchitecture/MyApp.Application/Orders/Commands/AddOI/AddOIHandler.cs
@@ -11,6 +11,12 @@ namespace MyApp.Application.Orders.Commands.AddOI
     {
         private readonly IOrderRepository _orderRepository;
 
+/*************  ✨ Windsurf Command ⭐  *************/
+        /// <summary>
+        /// Constructor for AddOIHandler
+        /// </summary>
+        /// <param name="orderRepository">IOrderRepository instance</param>
+/*******  4eb02db6-8be1-475c-a35b-9902631a91f7  *******/
         public AddOIHandler(IOrderRepository orderRepository)
         {
             _orderRepository = orderRepository;
