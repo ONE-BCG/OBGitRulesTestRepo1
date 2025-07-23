003c3ff78d03f9cbc6522de517394452a715ebc1
pawan-kumar-onebcg <pawan.kumar@onebcg.com>
2025-07-23 15:12:54 +0530

    Test HotFix Commit

--- CleanArchitecture/MyApp.Domain/Models/AllOrders.cs
+++ CleanArchitecture/MyApp.Domain/Models/AllOrders.cs
@@ -6,11 +6,30 @@ using Microsoft.VisualBasic;
 
 namespace MyApp.Domain.Models
 {
+    /// <summary>
+    /// Represents an order entity with basic order information
+    /// </summary>
     public class AllOrders
     {
+        /// <summary>
+        /// Gets or sets the unique identifier for the order
+        /// </summary>
         public int IOrderID { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the unique identifier for the patient associated with this order
+        /// </summary>
         public int IPatientID { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the DME (Durable Medical Equipment) identifier
+        /// </summary>
         public int IDMEID { get; set; } 
+        
+        /// <summary>
+        /// Gets or sets the timestamp when the order was created or last modified
+        /// Can be null if the timestamp is not available
+        /// </summary>
         public DateTime? DtDateStamp { get; set; }        
         
     }
