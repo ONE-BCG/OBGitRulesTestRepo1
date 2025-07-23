a21a5fc5ca0b3cbf5c1f90545fa9027f90e95618
ritik-bhatnagar-onebcg <ritik.bhatnagar@onebcg.com>
2025-07-23 14:54:39 +0530

    Test commit 1

--- CleanArchitecture/MyApp.Application/Orders/Commands/AddOI/AddOIRequestDto.cs
+++ CleanArchitecture/MyApp.Application/Orders/Commands/AddOI/AddOIRequestDto.cs
@@ -3,9 +3,17 @@ using MyApp.Domain.Models;
 
 namespace MyApp.Application.Orders.Commands.AddOI
 {
+    /// <summary>
+    /// Request DTO for adding order information.
+    /// Implements MediatR's IRequest interface to return an AddOIResponseDto.
+    /// </summary>
     public class AddOIRequestDto : IRequest<AddOIResponseDto>
     {
+        /// <summary>
+        /// The order information to be added.
+        /// This model contains the details needed to create a new order info entry.
+        /// </summary>
         public OIModel OrderInfo { get; set; }
-              
+
     }
 }
--- CleanArchitecture/MyApp.Application/Orders/Commands/AddOI/AddOIResponseDto.cs
+++ CleanArchitecture/MyApp.Application/Orders/Commands/AddOI/AddOIResponseDto.cs
@@ -5,8 +5,15 @@ using System.Threading.Tasks;
 
 namespace MyApp.Application.Orders.Commands.AddOI
 {
+    /// <summary>
+    /// Response DTO returned after successfully adding order information.
+    /// Contains the identifier of the newly created order info entry.
+    /// </summary>
     public class AddOIResponseDto
     {
+        /// <summary>
+        /// The unique identifier of the newly added order information record.
+        /// </summary>
         public int OrderInformationId { get; set; }
     }
 }
