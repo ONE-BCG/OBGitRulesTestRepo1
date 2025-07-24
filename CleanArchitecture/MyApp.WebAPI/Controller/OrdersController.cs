2f75b3d6bb5160d735e166d3c463dc0ad1bf742c
pawan-kumar-onebcg <pawan.kumar@onebcg.com>
2025-07-24 10:31:14 +0530

    Dev Test commit

--- CleanArchitecture/MyApp.WebAPI/Controller/OrdersController.cs
+++ CleanArchitecture/MyApp.WebAPI/Controller/OrdersController.cs
@@ -10,6 +10,10 @@ using System.Linq;
 
 namespace MyApp.WebAPI.Controller
 {
+    /// <summary>
+    /// API Controller for managing orders and order information
+    /// Provides endpoints for retrieving and creating order data
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     // [Authorize] 
@@ -18,10 +22,21 @@ namespace MyApp.WebAPI.Controller
 
         private readonly IMediator _mediator;
 
+        /// <summary>
+        /// Initializes a new instance of the OrdersController class
+        /// </summary>
+        /// <param name="mediator">The mediator instance for handling CQRS operations</param>
         public OrdersController(IMediator mediator)
         {
             _mediator = mediator;
         }
+        /// <summary>
+        /// Retrieves all orders from the system
+        /// </summary>
+        /// <returns>
+        /// An ActionResult containing a list of all orders
+        /// Returns 200 OK with the orders data on success
+        /// </returns>
         [HttpGet("GetAllOrders")]
         public async Task<IActionResult> GetAll()
         {
@@ -43,6 +58,15 @@ namespace MyApp.WebAPI.Controller
             }
         }
 
+        /// <summary>
+        /// Retrieves a specific order by its unique identifier
+        /// </summary>
+        /// <param name="id">The unique identifier of the order to retrieve. Must be greater than 0</param>
+        /// <returns>
+        /// An ActionResult containing the order details if found
+        /// Returns 200 OK with order data on success
+        /// Returns 400 Bad Request if the ID is invalid (less than or equal to 0)
+        /// </returns>
         [HttpGet("GetOrdersById")]
         public async Task<IActionResult> GetOrders(int id)
         {
@@ -69,6 +93,14 @@ namespace MyApp.WebAPI.Controller
             }
         }
 
+        /// <summary>
+        /// Creates new order information in the system
+        /// </summary>
+        /// <param name="request">The order information request containing the details to be added</param>
+        /// <returns>
+        /// An ActionResult containing the result of the order creation
+        /// Returns 200 OK with the created order information ID on success
+        /// </returns>
         [HttpPost("AddOrderInfo")]
         public async Task<IActionResult> AddOrderInfo([FromBody] AddOIRequestDto request)
         {
