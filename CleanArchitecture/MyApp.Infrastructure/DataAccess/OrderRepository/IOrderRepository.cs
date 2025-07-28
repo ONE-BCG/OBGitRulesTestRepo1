e325562809e1e9f01d87692f9d9bc7c40f98d475
pawan-kumar-onebcg <pawan.kumar@onebcg.com>
2025-07-28 15:44:35 +0530

    Dev to master test

--- CleanArchitecture/MyApp.Infrastructure/DataAccess/OrderRepository/IOrderRepository.cs
+++ CleanArchitecture/MyApp.Infrastructure/DataAccess/OrderRepository/IOrderRepository.cs
@@ -8,16 +8,20 @@ namespace MyApp.Infrastructure.DataAccess.OrderRepository
 {
     /// <summary>
     /// Repository contract for order data operations
+    /// /// This interface defines methods for retrieving and manipulating order data
+    /// in the underlying data store, such as a SQL Server database.
     /// </summary>
     public interface IOrderRepository
     {
         /// <summary>
         /// Gets all orders from the database
+        /// ///     This method retrieves a list of all orders using the stored procedure spGetAllOrders.
         /// </summary>
         Task<List<AllOrders>> GetAllOrdersAsync();
         
         /// <summary>
         /// Gets a specific order by its ID
+        /// ///     This method retrieves a single order using the stored procedure spGetOrdersById.
         /// </summary>
         Task<AllOrders> GetOrderByIdAsync(int orderId);
         
