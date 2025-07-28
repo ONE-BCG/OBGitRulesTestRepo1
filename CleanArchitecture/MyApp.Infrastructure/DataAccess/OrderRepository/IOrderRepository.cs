2f38259d9c63ecfee595ee9e1aff4094d602aaa3
pawan-kumar-onebcg <pawan.kumar@onebcg.com>
2025-07-28 15:40:14 +0530

    test Dev to master

--- CleanArchitecture/MyApp.Infrastructure/DataAccess/OrderRepository/IOrderRepository.cs
+++ CleanArchitecture/MyApp.Infrastructure/DataAccess/OrderRepository/IOrderRepository.cs
@@ -8,8 +8,13 @@ namespace MyApp.Infrastructure.DataAccess.OrderRepository
 {
     public interface IOrderRepository
     {
+        //This is the interface for order repository
+        //It defines the methods for data access operations related to orders
         Task<List<AllOrders>> GetAllOrdersAsync();
+        // Retrieves all orders from the database
         Task<AllOrders> GetOrderByIdAsync(int orderId);
+        // Retrieves a specific order by its ID
+        // Adds new order information to the database
         Task<int> AddOrderInfoAsync(OIModel oimodel);
     }
 }
